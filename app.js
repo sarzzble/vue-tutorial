e9d68d972c0205df15304a42776af112de7b45f0
Salih Demir <salihdemir436@gmail.com>
2025-07-05 14:00:11 +0300

    mouse events, outputting lists (v-for), attribute binding, dynamic classes, computed properties

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const app = Vue.createApp({
   // data, functions
-  // template: "<h2>I am the template</h2>",
   data() {
     return {
-      showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+      url: "https://vuejs.org/",
+      books: [
+        {
+          title: "The Fellowship of the Ring",
+          author: "J.R.R. Tolkien",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "The Two Towers",
+          author: "J.R.R. Tolkien",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "The Return of the King",
+          author: "J.R.R. Tolkien",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
+      x: 0,
+      y: 0,
     };
   },
   methods: {
-    changeTitle(title) {
-      //this.title = "The Well of Ascension";
-      this.title = title;
+    handleEvent(e, data) {
+      console.log(e, e.type);
+
+      if (data) {
+        console.log(data);
+      }
     },
-    toggleShowBooks() {
-      this.showBooks = !this.showBooks;
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      // Return only the books that are marked as favorite (isFav: true)
+      // This computed property is automatically updated when 'books' changes
+      // Vue caches the result unless 'books' is modified, improving performance
+      // It can be used like a normal variable inside the template
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
